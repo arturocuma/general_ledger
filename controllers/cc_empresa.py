6cffbf94fd115db8fd0a74dd6230f2cd7df2cb8e
Arturo Cruz Maya <arturocuma@gmail.com>
2014-07-03 12:59:01 -0500

    Wizard Cat Cuentas

--- controllers/cc_empresa.py
+++ controllers/cc_empresa.py
@@ -2,8 +2,30 @@
 # try something like
 def index(): return dict(message="hello from cc_empresa.py")
 
+def crear_cc(form):
+    if form.record:
+        form.vars.num_cc = form.vars.num_cc
+    elif (form.vars.num_cc != ''):
+        empresa_id=1
+        niveles_cc_empresa=db(db.niveles_cc_empresa.empresa_id==empresa_id).select()
+        niveles_cc=niveles_cc_empresa[0]
+        if form.vars.tipo_cc_id=='1':#Acumulativa
+            num_niv=int(niveles_cc['digitos_cc_acum'])
+        elif form.vars.tipo_cc_id=='2':#Auxiliar
+            num_niv=int(niveles_cc['digitos_cc_aux'])
+    
+        num_cc= form.vars.num_cc
+        str(num_cc).zfill(num_niv)
+        
+        form.vars.num_cc = form.vars.cuenta_padre+'.'+ num_cc
+        print form.vars
+    return
+
 def listar():
-    form = SQLFORM.smartgrid(db.cc_empresa, linked_tables=['empresa'])
+    form = SQLFORM.smartgrid(db.cc_empresa,
+                             onvalidation = crear_cc,
+                             editable = True,
+                             linked_tables=['empresa'])
     return dict(form=form)
 
 def resumen():
---
+++ controllers/wizard_cat_cuentas.py
@@ -0,0 +1,68 @@
+# coding: utf8
+# intente algo como
+
+import csv
+    
+def index(): 
+    cc_uno=cat_cuentas_nivel_uno(0)
+    cc_dos=cat_cuentas_nivel_dos(0)
+    cc_tres=cat_cuentas_nivel_tres(0)
+    return dict(cc_uno=cc_uno, cc_dos=cc_dos, cc_tres=cc_tres)
+
+def wiz_cc():
+    empresa_id=int(request.vars.empresa_id)
+    cc_preconf=int(request.vars.cc_preconf)
+    cc_conf=int(request.vars.cc_conf)
+    dig_acum=int(request.vars.dig_acum)
+    dig_aux=int(request.vars.dig_aux)
+    cc_uno=cat_cuentas_nivel_uno(empresa_id)
+    cc_dos=cat_cuentas_nivel_dos(empresa_id)
+    cc_tres=cat_cuentas_nivel_tres(empresa_id)
+    
+    if cc_preconf>=1:
+        cat_cuentas=cc_uno
+    if cc_preconf>=2:
+        cat_cuentas=cat_cuentas+cc_dos
+    if cc_preconf>=3:
+        cat_cuentas=cat_cuentas+cc_tres
+        cat_cuentas=cc_tres
+    
+    db(db.cc_empresa).delete()
+    db(db.niveles_cc_empresa.empresa_id==empresa_id).delete()
+    db.niveles_cc_empresa.insert(empresa_id=empresa_id, niveles=cc_conf+cc_preconf, digitos_cc_acum=dig_acum, digitos_cc_aux=dig_aux)
+
+    campos_cc=['empresa_id','cuenta_padre', 'num_cc','descripcion','nivel','tipo_naturaleza_id', 'tipo_cc_id']
+
+    for cuenta in cat_cuentas:
+        dict_cc= dict(zip(campos_cc, cuenta))
+        insert_cc=db[db.cc_empresa].insert(**dict_cc)
+
+    mensaje="Catálogo creado"    
+    return XML(mensaje)
+    
+def cat_cuentas_nivel_uno(empresa_id):    
+    nivel_uno=[]
+    with open('applications/fase1/private/cc_nivel_uno.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:        
+            row[0]=str(empresa_id)
+            nivel_uno.append(row)
+    return nivel_uno
+
+def cat_cuentas_nivel_dos(empresa_id):
+    nivel_dos=[]
+    with open('applications/fase1/private/cc_nivel_dos.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:        
+            row[0]=str(empresa_id)
+            nivel_dos.append(row)
+    return nivel_dos
+
+def cat_cuentas_nivel_tres(empresa_id):
+    nivel_tres=[]
+    with open('applications/fase1/private/cc_nivel_tres.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:        
+            row[0]=str(empresa_id)
+            nivel_tres.append(row)
+        return nivel_tres
