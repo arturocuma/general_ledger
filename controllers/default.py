3db64cbf87d063791aac2683594000de8c595408
Ricardo de la Cruz Arevalo <lsc.arevalo3010@gmail.com>
2014-08-18 14:26:56 -0500

    Cambio de ruta para los botones de las empresas

--- controllers/default.py
+++ controllers/default.py
@@ -13,6 +13,9 @@ import csv
 if session.instancias:
     db=empresas.dbs[int(session.instancias)]
 
+def a():
+    return dict()
+
 def insertar_pais(nombre):
     """
     Inserta un registro `pais` si no existe,
@@ -467,6 +470,10 @@ def index():
     from gluon.storage import Storage
     from uuid import uuid4
     from gluon.storage import Storage
+
+    empresa_id = request.args(0)
+    if empresa_id:
+        session.instancias = empresa_id
 
     if session.auth:
 
